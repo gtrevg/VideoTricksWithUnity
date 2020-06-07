9ed14310315b58e5f8aec8c98d0c8d79d17fb909
nablam <lamribenn@gmail.com>
2020-06-07 15:49:54 -0400

    2019.3

---
+++ UnityNdiProject/Assets/Kitchen_unity/KitchenCtrl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenCtrl : MonoBehaviour
+{
+
+    public GameObject IslandObj;
+    public GameObject FridgeDoorObj;
+    public GameObject CabL1Obj;
+    public GameObject CabCornObj;
+    public GameObject CabR1Obj;
+    public GameObject HauteObj;
+    public GameObject Barsool1Obj;
+    public GameObject Barsool2Obj; 
+    public GameObject Barsool3Obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
