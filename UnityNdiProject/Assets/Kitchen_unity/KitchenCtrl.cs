35a4a025cfb6e222b33e71ff9b48c55f853b6107
nablam <lamribenn@gmail.com>
2020-06-07 20:56:13 -0400

    linking kitchen objects, need to separate Rcabinet from room in blender

--- UnityNdiProject/Assets/Kitchen_unity/KitchenCtrl.cs
+++ UnityNdiProject/Assets/Kitchen_unity/KitchenCtrl.cs
@@ -11,9 +11,25 @@ public class KitchenCtrl : MonoBehaviour
     public GameObject CabCornObj;
     public GameObject CabR1Obj;
     public GameObject HauteObj;
+    public GameObject Trashcan;
+    public GameObject Fridge;
     public GameObject Barsool1Obj;
     public GameObject Barsool2Obj; 
     public GameObject Barsool3Obj;
+    public GameObject WoodStool;
+    public GameObject CoffeMaker;
+    public GameObject Nespresso;
+    public GameObject KnifeBlock;
+    public GameObject WoodenSpoonVase;
+    public GameObject WaterBoiler;
+    public GameObject Toaster;
+    public GameObject LargeYellowBowl;
+    public GameObject Britta;
+    public GameObject BrownSquareBasket;
+    public GameObject Lightbasket;
+    public GameObject SmallWhiteBowl;
+    public GameObject TissueBox;
+
     // Start is called before the first frame update
     void Start()
     {
